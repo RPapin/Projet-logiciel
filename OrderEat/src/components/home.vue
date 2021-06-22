9665324b833ca260e4c721fe733885dbba99fb31
fabuloup <fabuloup@gmail.com>
2021-06-22 14:00:44 +0200

    Fixed a syntax error on home.vue

--- OrderEat/src/components/home.vue
+++ OrderEat/src/components/home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row justify-content-center">
         <div class="col-md-6">
-            <h3 class="text-center">Bienvenu sur OrderEat</h3>
+            <h3 class="text-center">Bienvenue sur OrderEat</h3>
         </div>
     </div>
 </template>
