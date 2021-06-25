ee63e9c0e8668807037e44f1074305bc61ab635e
fabuloup <fabuloup@gmail.com>
2021-06-25 13:05:16 +0200

    Updated mongodb docker to use default mongodb image (build faster) and to load default data when building

---
+++ MongoDb/scripts/mongo-init-seed.js
@@ -0,0 +1,113 @@
+// Insert Datas
+db.restaurant.insertMany([
+  {
+    "_id": 0,
+    "manager_id" : "0",
+    "address" : "Centre Cial Ecully Grand Ouest, 69130 Écully",
+    "name" : "Restaurant Pizzeria d'Ecully",
+    "description" : "",
+    "tags" : "sur_place a_emporter aucune_livraison",
+    "picture" : ""
+  },
+  {
+    "_id": 1,
+    "manager_id" : "0",
+    "address" : "10 Route du Perollier, 69570 Dardilly",
+    "name" : "Les marches",
+    "description" : "",
+    "tags" : "sur_place aucune_livraison",
+    "picture" : ""
+  },
+  {
+    "_id": 2,
+    "manager_id" : "0",
+    "address" : "7 Place de l'Église, 69570 Dardilly",
+    "name" : "L'Auberge du Village",
+    "description" : "",
+    "tags" : "sur_place aucune_livraison",
+    "picture" : ""
+  }
+]);
+
+db.product.insert({
+  "_id": 0,
+  "restaurant_id" : 0,
+  "type" : "food",
+  "name" : "Pizza Reine",
+  "price" : "10,00€",
+  "estimation_time" : "40",
+  "picture" : ""
+});
+
+db.product.insert({
+  "_id": 1,
+  "restaurant_id" : 0,
+  "type" : "food",
+  "name" : "Pizza Carnivore",
+  "price" : "10,00€",
+  "estimation_time" : "40",
+  "picture" : ""
+});
+
+db.product.insert({
+  "_id": 2,
+  "restaurant_id" : 0,
+  "type" : "food",
+  "name" : "Pizza Hawaïenne",
+  "price" : "10,00€",
+  "estimation_time" : "40",
+  "picture" : ""
+});
+
+db.product.insert({
+  "_id": 3,
+  "restaurant_id" : 0,
+  "type" : "food",
+  "name" : "Pizza Norvegienne",
+  "price" : "10,00€",
+  "estimation_time" : "40",
+  "picture" : ""
+});
+
+db.product.insert({
+  "_id": 4,
+  "restaurant_id" : 0,
+  "type" : "drink",
+  "name" : "Coca-Cola",
+  "price" : "2,00€",
+  "estimation_time" : "0",
+  "picture" : ""
+});
+
+db.product.insert({
+  "_id": 5,
+  "restaurant_id" : 0,
+  "type" : "sauce",
+  "name" : "Sauce piquante",
+  "price" : "0,00€",
+  "estimation_time" : "0",
+  "picture" : ""
+});
+
+db.menu.insert({
+  "_id": 0,
+  "restaurant_id" : 0,
+  "name" : "Menu Pizza Reine",
+  "price" : "11,50€",
+  "estimation_time" : "40",
+  "picture" : "",
+  "menu_composition" : [
+    {
+      "product_id" : 0,
+      "quantity" : 1
+    },
+    {
+      "product_id" : 4,
+      "quantity" : 1
+    },
+    {
+      "product_id" : 5,
+      "quantity" : 4
+    }
+  ]
+});
---
+++ MongoDb/scripts/mongo-init.js
@@ -0,0 +1,228 @@
+// Create Collections
+db.createCollection( "restaurant",
+   {
+     capped: false,
+     autoIndexId: true,
+     validator: {
+       $jsonSchema : {
+         bsonType : ["object"],
+         title : "restaurant",
+         description : "restaurants collection",
+         properties : {
+           _id : {
+             bsonType : ["objectId"]
+           },
+           manager_id : {
+             bsonType : ["int"]
+           },
+           address : {
+             bsonType : ["string"]
+           },
+           name : {
+             bsonType : ["string"]
+           },
+           description : {
+             bsonType : ["string"]
+           },
+           tags : {
+             bsonType : ["string"]
+           },
+           picture : {
+             bsonType : ["string"]
+           }
+         }
+       }
+      }
+   }
+);
+
+db.createCollection( "product",
+   {
+     capped: false,
+     autoIndexId: true,
+     validator: {
+       $jsonSchema : {
+         bsonType : ["object"],
+         title : "product",
+         description : "products collection",
+         properties : {
+           _id : {
+             bsonType : ["objectId"]
+           },
+           restaurant_id : {
+             bsonType : ["objectId"]
+           },
+           type : {
+             bsonType : ["string"],
+             enum : [
+               "food",
+               "sauce",
+               "drink"
+             ]
+           },
+           name : {
+             bsonType : ["string"]
+           },
+           price : {
+             bsonType : "float"
+           },
+           estimation_time : {
+             bsonType : ["int"]
+           },
+           picture : {
+             bsonType : ["string"]
+           }
+         }
+       }
+       }
+   }
+);
+
+db.createCollection( "menu",
+   {
+     capped: false,
+     autoIndexId: true,
+     validator: {
+       $jsonSchema : {
+         bsonType : "object",
+         title : "menu",
+         description : "menus collection",
+         properties : {
+           _id : {
+             bsonType : ["objectId"]
+           },
+           restaurant_id : {
+             bsonType : ["objectId"]
+           },
+           name : {
+             bsonType : ["string"]
+           },
+           price : {
+             bsonType : "float"
+           },
+           estimation_time : {
+             bsonType : ["int"]
+           },
+           picture : {
+             bsonType : ["string"]
+           },
+           menu_composition : {
+              bsonType: ["array"],
+              minItems: 1, // each menu must have at least one product
+              uniqueItems: true,
+              additionalProperties: false,
+              items: {
+                  bsonType: ["object"],
+                  required: ["product_id", "quantity"],
+                  additionalProperties: false,
+                  description: "",
+                  properties: {
+                      product_id: {
+                        bsonType: ["objectId"]
+                      },
+                      quantity: {
+                        bsonType: ["int"]
+                      }
+                  }
+              }
+           }
+         }
+       }
+     }
+   }
+);
+
+db.createCollection( "order",
+   {
+     capped: false,
+     autoIndexId: true,
+     validator: {
+       $jsonSchema : {
+         bsonType : "object",
+         title : "order",
+         description : "orders collection",
+         properties : {
+           _id : {
+             bsonType : ["objectId"]
+           },
+           restaurant_id : {
+             bsonType : ["objectId"]
+           },
+           client_id : {
+             bsonType : ["int"]
+           },
+           state : {
+             bsonType : ["string"],
+             enum : [
+               "placing",
+               "validation",
+               "delivery accepted",
+               "delivery received"
+             ]
+           },
+           qrcode : {
+             bsonType : "string"
+           },
+           order_items : {
+             bsonType: ["array"],
+              minItems: 1, // each menu must have at least one product
+              uniqueItems: true,
+              additionalProperties: false,
+              items: {
+                  bsonType: ["object"],
+                  required: ["item_id", "item_type", "quantity"],
+                  additionalProperties: false,
+                  description: "",
+                  properties: {
+                      item_id: {
+                        bsonType: ["objectId"]
+                      },
+                      item_type: {
+                        bsonType: ["string"],
+                        enum: [
+                          "menu",
+                          "product"
+                        ]
+                      },
+                      quantity: {
+                        bsonType: ["int"]
+                      }
+                  }
+              }
+           }
+         }
+       }
+     }
+   }
+);
+
+db.createCollection( "evaluation",
+   {
+     capped: false,
+     autoIndexId: true,
+     validator: {
+            $jsonSchema : {
+              bsonType : "object",
+              title : "evaluation",
+              description : "evaluations collection",
+              properties : {
+                _id : {
+                  bsonType : ["objectId"]
+                },
+                restaurant_id : {
+                  bsonType : ["objectId"]
+                },
+                order_id : {
+                  bsonType : ["objectId"]
+                },
+                client_id : {
+                  bsonType : ["int"]
+                },
+                note : {
+                  bsonType : ["int"]
+                }
+              }
+            }
+     }
+   }
+);
