556a40e7573aaa4efb3224727acd180ad3930248
Fabuloup <fabuloup@gmail.com>
2021-06-28 15:29:47 +0200

    Fixed Redis host problem

--- backend-typescript/src/redis.config.ts
+++ backend-typescript/src/redis.config.ts
@@ -1,6 +1,7 @@
 import redis from 'redis'
+import {environment} from './environment'
 
-const redisClient = redis.createClient(6379);
+const redisClient = redis.createClient(6379, environment.redisHost);
 redisClient.on('error', (err) => {
     console.log('Error occured while connecting or accessing redis server');
 });
