6d23e26a7b15fa7670a1eb56c74a0362570ca098
corentus12345 <corentin.brion@viacesi.fr>
2021-06-29 09:17:14 +0200

    C# Application

---
+++ Application/WpfApp1/WpfApp1/WpfApp1/BDD.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace WpfApp1
+{
+
+    public static class SqlHelper
+    {
+        public static string GetValueAsString(this SqlDataReader reader, string columnName)
+        {
+            object val = reader[columnName];
+
+            if (val == DBNull.Value)
+            {
+                return "";
+            }
+
+            return val.ToString();
+        }
+    }
+    public class User
+    {
+        private int id;
+        private string lastName;
+        private string firstName;
+        private string sponsorship;
+        private string role;
+        private string phoneNumber;
+        private string email;
+        private string password;
+        public User(int id, string lastName, string firstName, string sponsorship, string role, string phoneNumber, string email, string password)
+        {
+            this.id = id;
+            this.lastName = lastName;
+            this.firstName = firstName;
+            this.sponsorship = sponsorship == "" ? "NULL" : sponsorship;
+            this.role = role;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+            this.password = password;
+        }
+
+        public User(Dictionary<string, string> data)
+        {
+            this.id = Int32.Parse(data["id"]);
+            this.lastName = data["lastName"];
+            this.firstName = data["firstName"];
+            this.sponsorship = data["sponsorship"] == "" ? "NULL" : data["sponsorship"];
+            this.role = data["role"];
+            this.phoneNumber = data["phoneNumber"];
+            this.email = data["email"];
+            this.password = data["password"];
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string Sponsorship { get => sponsorship; set => sponsorship = value; }
+        public string Role { get => role; set => role = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+    }
+
+    // TODO: Close?
+    static class BDD
+    {
+        private static string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnectionString"].ConnectionString;
+
+        private static User CreateUserFromReader(SqlDataReader reader)
+        {
+            Dictionary<string, string> data = new Dictionary<string, string>();
+
+            data["id"] = reader.GetValueAsString("account_id");
+            data["lastName"] = reader.GetValueAsString("last_name");
+            data["firstName"] = reader.GetValueAsString("first_name");
+            data["sponsorship"] = reader.GetValueAsString("sponsorship");
+            data["role"] = reader.GetValueAsString("role");
+            data["phoneNumber"] = reader.GetValueAsString("phone_number");
+            data["email"] = reader.GetValueAsString("email");
+            data["password"] = reader.GetValueAsString("password");
+
+            return new User(data);
+        }
+
+        public static bool IsUserUnique(User user)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+
+            SqlCommand cmd;
+            SqlDataReader reader;
+            string sql = "Select Count(*) from Account where email = '" + user.Email + "' AND account_id != " + user.Id;
+
+            cmd = new SqlCommand(sql, con);
+            reader = cmd.ExecuteReader();
+
+            reader.Read();
+            int userCount = Int32.Parse(reader[0].ToString());
+
+            cmd.Dispose();
+            reader.Close();
+            con.Close();
+
+            return userCount == 0;
+        }
+
+        public static User GetUserById(int id)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+
+            SqlCommand cmd;
+            SqlDataReader reader;
+            string sql = "Select account_id, last_name, first_name, sponsorship, role_id as role, phone_number, email, password from Account where account_id = " + id;
+
+            cmd = new SqlCommand(sql, con);
+            reader = cmd.ExecuteReader();
+
+            reader.Read();
+            User user = CreateUserFromReader(reader);
+
+            cmd.Dispose();
+            reader.Close();
+            con.Close();
+
+            return user;
+        }
+
+
+        public static List<User> GetAllUsers()
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+
+            List<User> users = new List<User>();
+
+            SqlCommand cmd;
+            SqlDataReader reader;
+            string sql = "Select account_id, last_name, first_name, sponsorship, role_name as role, phone_number, email, password from Account a Left JOIN Role r ON a.role_id = r.role_id;";
+
+            cmd = new SqlCommand(sql, con);
+            reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                users.Add(CreateUserFromReader(reader));
+            }
+
+            cmd.Dispose();
+            reader.Close();
+            con.Close();
+
+            return users;
+        }
+
+        public static void InsertUser(User user)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+
+            string values = String.Format("'{0}', '{1}', '{2}', '{3}', '{4}', {5}, {6}", user.LastName, user.FirstName, user.PhoneNumber, user.Email, user.Password, user.Sponsorship, user.Role);
+            string sql = "INSERT INTO Account (last_name, first_name, phone_number, email, password, sponsorship, role_id) VALUES (" + values + ")";
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            adapter.InsertCommand = new SqlCommand(sql, con);
+            adapter.InsertCommand.ExecuteNonQuery();
+
+            adapter.Dispose();
+            con.Close();
+        }
+
+        public static void EditUser(User user)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+
+            string sql = "UPDATE Account SET last_name = '" + user.LastName + "', first_name = '" + user.FirstName + "', phone_number = '" + user.PhoneNumber + "', " +
+                "email = '" + user.Email + "', password = '" + user.Password + "', sponsorship = " + user.Sponsorship + ", role_id = " + user.Role + " where account_id = " + user.Id;
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            adapter.InsertCommand = new SqlCommand(sql, con);
+            adapter.InsertCommand.ExecuteNonQuery();
+
+            adapter.Dispose();
+            con.Close();
+        }
+
+        public static void DeleteUser(User user)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+
+            string sql = "DELETE FROM Account WHERE account_id = " + user.Id;
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            adapter.DeleteCommand = new SqlCommand(sql, con);
+            adapter.DeleteCommand.ExecuteNonQuery();
+
+            adapter.Dispose();
+            con.Close();
+        }
+    }
+}
---
+++ Application/WpfApp1/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,447 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Data;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();            
+            SwitchToMainPanel();
+        }
+
+        private void LoadEditForm(int id)
+        {
+            User user = BDD.GetUserById(id);
+            TextBoxEditLastName.Text = user.LastName;
+            TextBoxEditFirstName.Text = user.FirstName;
+            TextBoxEditSponsorship.Text = user.Sponsorship;
+            TextBoxEditRole.Text = user.Role;
+            TextBoxEditPhoneNumber.Text = user.PhoneNumber;
+            TextBoxEditEmail.Text = user.Email;
+            PasswordBoxEditPassword.Password = user.Password;
+            PasswordBoxEditConfirmPassword.Password = user.Password;
+            
+        }
+        private void ClearAddForm()
+        {
+            IEnumerable<TextBox> textBoxCollection = AddGrid.Children.OfType<TextBox>();
+            foreach (TextBox textBox in textBoxCollection)
+            {
+                textBox.Text = "";
+            }
+            EmailError.Visibility = Visibility.Hidden;
+
+            IEnumerable<PasswordBox> passwordBoxCollection = AddGrid.Children.OfType<PasswordBox>();
+            foreach (PasswordBox passwordBox in passwordBoxCollection)
+            {
+                passwordBox.Password = "";
+            }
+            PasswordError.Visibility = Visibility.Hidden;
+        }
+
+        private void ClearEditForm()
+        {
+            EditEmailError.Visibility = Visibility.Hidden;
+            EditPasswordError.Visibility = Visibility.Hidden;
+        }
+
+        private void LoadUserList()
+        {
+            List<User> info = BDD.GetAllUsers();
+            MainData.ItemsSource = info;
+        }
+
+        private void SwitchToMainPanel()
+        {
+            LoadUserList();
+            MainPanel.Visibility = Visibility.Visible;
+            AddPanel.Visibility = Visibility.Hidden;
+            EditPanel.Visibility = Visibility.Hidden;
+        }
+
+        private void SwitchToAddPanel()
+        {
+            MainPanel.Visibility = Visibility.Hidden;
+            AddPanel.Visibility = Visibility.Visible;
+            EditPanel.Visibility = Visibility.Hidden;
+        }
+
+        private void SwitchToEditPanel()
+        {
+            MainPanel.Visibility = Visibility.Hidden;
+            AddPanel.Visibility = Visibility.Hidden;
+            EditPanel.Visibility = Visibility.Visible;
+        }
+
+        private void MainData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearAddForm();
+            SwitchToAddPanel();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainData.SelectedItems.Count == 0)
+            {
+                System.Windows.MessageBox.Show("Veuillez sélectionner une ou plusieurs lignes", "Erreur");
+                return;
+            }
+
+            MessageBoxResult result = System.Windows.MessageBox.Show("Êtes-vous sûr de vouloir supprimer ce ou ces utilisateur(s)", "Confirmation", System.Windows.MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.No)
+            {
+                return;
+            }
+
+            List<User> usersToRemove = MainData.SelectedItems.Cast<User>().ToList();
+
+            foreach (User user in usersToRemove)
+            {
+                BDD.DeleteUser(user);
+            }
+
+            LoadUserList();
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainData.SelectedItems.Count != 1)
+            {
+                System.Windows.MessageBox.Show("Veuillez sélectionner une seule ligne", "Information");
+                return;
+            }
+
+            User user = (User)MainData.SelectedItem;
+            LoadEditForm(user.Id);
+            SwitchToEditPanel();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchToMainPanel();
+            ClearAddForm();
+        }
+
+        private bool CheckAddForm()
+        {
+            bool isValid = true;
+            isValid &= CheckAddFormMissingFields();
+            isValid &= CheckAddFormErrors();
+            return isValid;
+        }
+
+        private bool CheckAddFormMissingFields()
+        {
+            bool isValid = true;
+            List<TextBox> requiredTextBoxFields = new List<TextBox>()
+            {
+                TextBoxLastName, TextBoxFirstName, TextBoxRole,
+                TextBoxPhoneNumber, TextBoxEmail,
+            };
+
+            foreach (TextBox textBox in requiredTextBoxFields)
+            {
+                if (textBox.Text.Trim() == "")
+                {
+                    isValid = false;
+                    textBox.Background = Brushes.Red;
+                    // Show error? (outline field in red?)
+                } else
+                {
+                    textBox.Background = Brushes.White;
+                }
+            }
+
+            if (PasswordBoxPassword.Password.Trim() == "")
+            {
+                isValid = false;
+                PasswordBoxPassword.Background = Brushes.Red;
+                // Show error? (outline field in red?)
+            } else
+            {
+                PasswordBoxPassword.Background = Brushes.White;
+            }
+
+            return isValid;
+        }
+
+        private bool CheckAddFormErrors()
+        {
+            bool isValid = true;
+            string password = PasswordBoxPassword.Password;
+            string confirmPassword = PasswordBoxConfirmPassword.Password;
+            if (password != confirmPassword)
+            {
+                PasswordError.Visibility = Visibility.Visible;
+                isValid = false;
+            }
+            else
+            {
+                PasswordError.Visibility = Visibility.Hidden;
+            }
+
+            string email = TextBoxEmail.Text;
+            if (!Validations.isEmailValid(email))
+            {
+                EmailError.Visibility = Visibility.Visible;
+                isValid = false;
+            }
+            else
+            {
+                EmailError.Visibility = Visibility.Hidden;
+            }
+
+            return isValid;
+        }
+
+        private bool CheckEditForm()
+        {
+            bool isValid = true;
+            isValid &= CheckEditFormMissingFields();
+            isValid &= CheckEditFormErrors();
+            return isValid;
+        }
+
+        private bool CheckEditFormMissingFields()
+        {
+            bool isValid = true;
+            List<TextBox> requiredTextBoxFields = new List<TextBox>()
+            {
+                TextBoxEditLastName, TextBoxEditFirstName, TextBoxEditRole,
+                TextBoxEditPhoneNumber, TextBoxEditEmail,
+            };
+
+            foreach (TextBox textBox in requiredTextBoxFields)
+            {
+                if (textBox.Text.Trim() == "")
+                {
+                    isValid = false;
+                    textBox.Background = Brushes.Red;
+                    // Show error? (outline field in red?)
+                } else
+                {
+                    textBox.Background = Brushes.White;
+                }
+            }
+
+            if (PasswordBoxEditPassword.Password.Trim() == "")
+            {
+                isValid = false;
+                PasswordBoxEditPassword.Background = Brushes.Red;
+                // Show error? (outline field in red?)
+            } else
+            {
+                PasswordBoxPassword.Background = Brushes.White;
+            }
+
+            return isValid;
+        }
+
+        private bool CheckEditFormErrors()
+        {
+            bool isValid = true;
+            string password = PasswordBoxEditPassword.Password;
+            string confirmPassword = PasswordBoxEditConfirmPassword.Password;
+            if (password != confirmPassword)
+            {
+                EditPasswordError.Visibility = Visibility.Visible;
+                isValid = false;
+            }
+            else
+            {
+                EditPasswordError.Visibility = Visibility.Hidden;
+            }
+
+            string email = TextBoxEditEmail.Text;
+            if (!Validations.isEmailValid(email))
+            {
+                EditEmailError.Visibility = Visibility.Visible;
+                isValid = false;
+            }
+            else
+            {
+                EditEmailError.Visibility = Visibility.Hidden;
+            }
+
+            return isValid;
+        }
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!CheckAddForm())
+            {
+                EmailAlreadyInUseError.Visibility = Visibility.Hidden;
+                return;
+            }
+
+            Dictionary<string, string> userData = new Dictionary<string, string>();
+
+            userData["id"] = "-1";
+            userData["lastName"] = TextBoxLastName.Text;
+            userData["firstName"] = TextBoxFirstName.Text;
+            userData["sponsorship"] = TextBoxSponsorship.Text;
+            userData["role"] = TextBoxRole.Text;
+            userData["phoneNumber"] = TextBoxPhoneNumber.Text;
+            userData["email"] = TextBoxEmail.Text;
+            userData["password"] = PasswordBoxPassword.Password;
+
+            User userToInsert = new User(userData);
+
+            if (!BDD.IsUserUnique(userToInsert))
+            {
+                // Display error -> user is not unique
+                EmailAlreadyInUseError.Visibility = Visibility.Visible;
+                return;
+            }
+
+            BDD.InsertUser(userToInsert);
+
+            SwitchToMainPanel();
+        }
+
+        private void EditConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!CheckEditForm())
+            {
+                EditEmailAlreadyInUseError.Visibility = Visibility.Hidden;
+                return;
+            }
+
+            Dictionary<string, string> userData = new Dictionary<string, string>();
+
+            userData["id"] = ((User)MainData.SelectedItem).Id.ToString();
+            userData["lastName"] = TextBoxEditLastName.Text;
+            userData["firstName"] = TextBoxEditFirstName.Text;
+            userData["sponsorship"] = TextBoxEditSponsorship.Text;
+            userData["role"] = TextBoxEditRole.Text;
+            userData["phoneNumber"] = TextBoxEditPhoneNumber.Text;
+            userData["email"] = TextBoxEditEmail.Text;
+            userData["password"] = PasswordBoxEditPassword.Password;
+
+            User updatedUser = new User(userData);
+
+            if (!BDD.IsUserUnique(updatedUser))
+            {
+                // Display error -> user is not unique
+                EditEmailAlreadyInUseError.Visibility = Visibility.Visible;
+                return;
+            }
+
+            BDD.EditUser(updatedUser);
+
+            SwitchToMainPanel();
+            ClearEditForm();
+        }
+
+        private void EditCancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchToMainPanel();
+            ClearEditForm();
+        }
+
+        private void TextBoxEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBoxEmail.Background = Brushes.White;
+            EmailAlreadyInUseError.Visibility = Visibility.Hidden;
+
+            string email = TextBoxEmail.Text;
+            if (!Validations.isEmailValid(email))
+            {
+                EmailError.Visibility = Visibility.Visible;
+            } else
+            {
+                EmailError.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void TextBoxEditEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBoxEditEmail.Background = Brushes.White;
+            EditEmailAlreadyInUseError.Visibility = Visibility.Hidden;
+
+            string email = TextBoxEditEmail.Text;
+            if (!Validations.isEmailValid(email))
+            {
+                EditEmailError.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                EditEmailError.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void TextBoxLastName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBoxLastName.Background = Brushes.White;
+        }
+
+        private void TextBoxFirstName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBoxFirstName.Background = Brushes.White;
+        }
+
+        private void TextBoxPhoneNumber_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBoxPhoneNumber.Background = Brushes.White;
+        }
+
+        private void TextBoxSponsorship_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void TextBoxRole_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBoxRole.Background = Brushes.White;
+        }
+
+        private void PasswordBoxPassword_Changed(object sender, RoutedEventArgs e)
+        {
+            PasswordBoxPassword.Background = Brushes.White;
+        }
+
+        private void TextBoxEditLastName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBoxEditLastName.Background = Brushes.White;
+        }
+
+        private void TextBoxEditFirstName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBoxEditFirstName.Background = Brushes.White;
+        }
+
+        private void TextBoxEditPhoneNumber_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBoxEditPhoneNumber.Background = Brushes.White;
+        }
+
+        private void TextBoxEditSponsorship_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBoxEditRole_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBoxEditRole.Background = Brushes.White;
+        }
+
+        private void PasswordBoxEditPassword_Changed(object sender, RoutedEventArgs e)
+        {
+            PasswordBoxEditPassword.Background = Brushes.White;
+        }
+    }
+}
