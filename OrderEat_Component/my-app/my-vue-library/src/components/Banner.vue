18b5edd3e8bea6a90e79e45325ea71d0aea42638
corentus12345 <corentin.brion@viacesi.fr>
2021-06-30 20:08:17 +0200

    OrderEat_Component

---
+++ OrderEat_Component/my-app/my-vue-library/src/components/Banner.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="banner" :style="bannerStyles" :class="`banner__${position}`">
+    <slot></slot>
+  </div>
+</template>
+<script>
+const defaultStyles = {
+  left: 0,
+  right: 0,
+  padding: "12px",
+  color: "#f00",
+  position: "fixed"
+};
+export default {
+  props: {
+    position: {
+      type: String,
+      default: 'top',
+      validator(position) {
+        return ['top', 'bottom'].indexOf(position) > -1;
+      },
+    },
+    styles: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data() {
+    return {
+      bannerStyles: {
+        ...defaultStyles,
+        ...this.styles,
+      },
+    };
+  },
+};
+</script>
