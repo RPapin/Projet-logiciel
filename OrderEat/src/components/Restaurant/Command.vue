be64ba4ce8019406daebb96e6380de4a16c47dac
fabuloup <fabuloup@gmail.com>
2021-06-30 14:46:17 +0200

    Added client route and vue ressources

---
+++ OrderEat/src/components/Restaurant/Command.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <ListProductClient :restaurantId="restaurant_id"/>
+    <ListMenuClient :restaurantId="restaurant_id"/>
+  </div>
+</template>
+<script lang="ts">
+  import Vue from 'vue';
+  import ApiService from "../../services/apiService"
+  import { mapMutations, mapState, mapActions } from 'vuex'
+  import ListProductClient from '../Product/ListProductClient.vue'
+  import ListMenuClient from '../Menu/ListMenuClient.vue'
+
+export default Vue.extend({
+      data() {
+          return {
+              restaurant_id : this.$route.params.restaurant_id
+          }
+      },
+      async created() {
+        const service = new ApiService()
+
+        let apiURL:string = 'restaurants';
+        let authToken:string = localStorage.getItem('AUTH_TOKEN') === null ? "" : localStorage.getItem('AUTH_TOKEN')
+        let data:any = await service.getCall(apiURL, authToken);
+        this.Restaurants = data.restaurants
+      },
+      components: {
+        ListProductClient,
+        ListMenuClient
+      },
+      computed: mapState([
+        // map this.count to store.state.count
+        'isLoggedIn',
+        'userInfo'
+      ]),
+      methods: {
+        ...mapMutations([
+          'toggleIsLoggedIn', 
+          'updateUserInfo'
+        ]),
+        ...mapActions([
+          'checkUser',
+        ])
+    }
+  })  
+</script>
